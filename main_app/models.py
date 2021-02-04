96aacf4d64b6bc2f1ee6c575be5d4c822508865d
Mbanks1 <michael1banks@icloud.com>
2021-02-04 12:57:51 -0400

    referencing author

--- main_app/models.py
+++ main_app/models.py
@@ -4,4 +4,9 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=1500)
+    content = models.TextField()
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title + '|' + self.author
+    
