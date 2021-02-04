925bdc1d85180535744ca12f844cc9c186409b2f
Mbanks1 <michael1banks@icloud.com>
2021-02-04 12:27:27 -0400

    superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+#rror Create your models here.
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1500)
