495d7ee73abdb0802a9dc52a6ccd4e3742b31d2f
Mbanks1 <michael1banks@icloud.com>
2021-02-04 11:18:26 -0400

    index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('posts/', views.posts_index, name='index')
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def posts_index(request):
+    return render(request, 'posts/index.html')
