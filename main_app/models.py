72bf1dfceb29bb6d9b3da97a97d24364f41a0d96
Mbanks1 <michael1banks@icloud.com>
2021-02-07 13:05:24 -0500

    bootstrap added

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,7 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'post_id': self.id}) 
+        return reverse('index', kwargs={'pk': self.id}) 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
